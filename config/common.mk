85afe35b8661dac8c643cf6529087cfd3cf459bc
naz664 <nazimnavas145@gmail.com>
2024-12-24 05:12:51 +0000

    vendor: Import new bootanimation
    Signed-off-by: naz664 <nazimnavas145@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -275,3 +275,6 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Bootanimation
+include vendor/lineage/config/bootanimation.mk
